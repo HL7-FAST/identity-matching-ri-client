375960a878ff902aac2dba2c8c0b26e54f5f44f9
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-07-13 15:43:31 -0400

    building client

--- app/controllers/patient_server_controller.rb
+++ app/controllers/patient_server_controller.rb
@@ -4,13 +4,14 @@ class PatientServerController < ApplicationController
   def create
 	@patient_server = PatientServer.find_or_create_by!(patient_server_params)
 	session[:base] = @patient_server.base
-	redirect_to '/', notice: "Patient server set to #{session[:base]}."
+	redirect_to new_identity_matching_request_path, notice: "Patient server set to #{session[:base]}."
   end
 
   private
 
   def patient_server_params
 	params.require(:patient_server).permit([:base])
+	# TODO: normalize URL base
   end
 
 end
