62a69289d432bfd848b74076276fd39f65d856e5
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-07-15 12:55:42 -0400

    move resource into modle constant

--- app/models/identity_matching_request.rb
+++ app/models/identity_matching_request.rb
@@ -4,6 +4,53 @@ class IdentityMatchingRequest < ApplicationRecord
 
   serialize :response_json, JSON
 
+
+  MATCH_PARAMETER = <<~EOS
+	{
+	  "resourceType": "Parameters",
+	  "id": "%{id}",
+	  "parameter": [
+	    {
+	      "name": "resource",
+	      "resource": {
+	        "resourceType": "Patient",
+	        "identifier": [
+	          {
+	            "use": "usual",
+	            "type": {
+	              "coding": [
+	                {
+	                  "system": "http://hl7.org/fhir/v2/0203",
+	                  "code": "MR"
+	                }
+	              ]
+	            },
+	            "system": "urn:oid:1.2.36.146.595.217.0.1",
+	            "value": "12345"
+	          }
+	        ],
+	        "name": [
+	          {
+				"text": "%{name}"
+	          }
+	        ],
+	        "gender": "male",
+	        "birthDate": "1974-12-25"
+	      }
+	    },
+	    {
+	      "name": "count",
+	      "valueInteger": "3"
+	    },
+	    {
+	      "name": "onlyCertainMatches",
+	      "valueBoolean": "false"
+	    }
+	  ]
+	}
+  EOS
+
+
   # return pretty string for address
   def address
 	return "#{address_line1} #{address_line2}\n#{city} #{state} #{zipcode}"
@@ -13,9 +60,13 @@ class IdentityMatchingRequest < ApplicationRecord
   # returns:
   # 	FHIR::Model instance of IDIPatient profile
   def request_fhir
-	idi_patient_hash = {}
-	# TODO: build fhir resource
-	return FHIR.from_contents( idi_patient_hash.to_json )
+	idi_patient_json = IdentityMatchingRequest::MATCH_PARAMETER % {id: self.id, name: self.full_name}
+
+	puts "==="
+	puts idi_patient_json
+	puts "==="
+
+	return FHIR.from_contents( idi_patient_json )
   end
 
   # connect to a url and save response
