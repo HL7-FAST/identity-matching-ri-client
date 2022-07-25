d880ada57d15984623504f7e128d353ada525ff4
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-07-25 11:59:02 -0400

    add UDAP and OAuth to inflections

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   root 'welcome#index'
-
   get 'welcome/index'
+  get 'index', {controller: :welcome_controller, action: :index}
+
   get 'patient_server', {controller: :patient_server, action: :show}
   post 'patient_servers', {controller: :patient_server, action: :create}
+
   get 'identity_matching_requests/example'
   resources :identity_matching_requests
 
