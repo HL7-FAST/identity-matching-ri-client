29df1fa76e76e2107b7c4ffb4273b20cc2dff6c2
Shaumik-Ashraf <shaumikashraf@mitre.org>
2022-07-29 10:25:08 -0400

    add minimal regex validator test

--- test/models/identity_matching_test.rb
+++ test/models/identity_matching_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class IdentityMatchingTest < ActiveSupport::TestCase
+  setup do
+	@im1 = identity_matchings(:one)
+	@im2 = identity_matchings(:two)
+  end
+
   test "IdentityMatching model exists" do
     assert IdentityMatching
   end
+
+  test "IdentityMatching fixtures validated" do
+	assert @im1
+	assert @im2
+  end
+
 end
